66472ead529faf367bdef0b0a6679b3f8d8f4aee
Simon Branford <4967+branfosj@users.noreply.github.com>
2022-12-14 09:32:36 +0000

    activate

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,14 +1,10 @@
-# This script is intended to be sourced. It will set up a virtualenv
-# if one doesn't already exist, and leave you ready to run the scripts
-# in this folder.
+# This script is intended to be sourced. It will leave you
+# ready to run the scripts in this folder.
 
 HERE=$(dirname $(realpath ${BASH_SOURCE}))
 
 if ! [[ -e ${HERE}/venv ]]; then
-    python3 -m venv ${HERE}/venv
-    source ${HERE}/venv/bin/activate
-    pip install --upgrade pip
-    pip install -e ${HERE}/..
+    echo "Please set up a virtual environment (${HERE}/venv) and install this package and required db dependency."
 else
     source ${HERE}/venv/bin/activate
 fi
